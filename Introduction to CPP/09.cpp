acf7da4e9863fd556bccbaef8ecea7edb1307eb5
Jimin Kim <0305jimin@gmail.com>
2022-09-24 14:41:47 +0900

    Refactor: checking if the file can be opened

--- Introduction to CPP/09.cpp
+++ Introduction to CPP/09.cpp
@@ -2,14 +2,21 @@
 #include <fstream>
 using namespace std;
 
-int main() {
+int main()
+{
     streampos begin, end; // can be considered as integer
-    ifstream myfile("example.bin",ios::binary);
-    begin = myfile.tellg(); // 0
-    myfile.seekg(0,ios::end); // move the position to ios::end
-    end = myfile.tellg(); // end of the doc
-    myfile.close(); 
-    cout << "size is : " << (end-begin) << " bytes.\n";
+    ifstream myfile("example.bin", ios::binary);
 
-    return 0; 
+    if (myfile.is_open())
+    {
+        begin = myfile.tellg();    // 0
+        myfile.seekg(0, ios::end); // move the position to ios::end
+        end = myfile.tellg();      // end of the doc
+        myfile.close();
+        cout << "size is : " << (end - begin) << " bytes.\n";
+    }
+    else
+        cout << "Unable to open file";
+
+    return 0;
 }
