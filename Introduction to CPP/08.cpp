90522ddf130bb9b258f3b601d2d4e3fc1fac438d
Jimin Kim <0305jimin@gmail.com>
2022-10-20 16:47:36 +0900

    Docs: annotating; mode for opening file and catching an error

--- Introduction to CPP/08.cpp
+++ Introduction to CPP/08.cpp
@@ -6,12 +6,17 @@ using namespace std;
 int main() {
     string line;
     ifstream myfile ("example.txt");
-    if (myfile.is_open()){
+    /* 
+        since the mode is ios::in(default), 
+        if there's no corresponding file, 
+        an error occurs. so the if statement is needed
+    */
+    if (myfile.is_open()){ // if there exists a file called "example.txt"
         while (getline (myfile, line)){
             cout << line << '\n';
         }
         myfile.close();
-    }else cout << "Unable to open file";
+    }else cout << "Unable to open file"; // if there's no file called "example.txt"
 
     return 0;
 }
