81eaad3565d5ca12c9d0b2658264944b93a5cf64
Jimin Kim <0305jimin@gmail.com>
2022-10-20 14:00:37 +0900

    Docs : annotating; compilation, standard io

--- Introduction to CPP/01.cpp
+++ Introduction to CPP/01.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
-using namespace std; // lots of functions and objects are designed in std
+#include <iostream> // standard input and output
+using namespace std; // "we are gonna use functions and objects that are in std"
 
 int main(){
-    cout << "Hello World!\n"; // cout : standard output (screen output)
+    cout << "Hello World!\n"; // cout : standard output (screen stream object)
     cout << "I'm a C++ program\n";
 
     int i;
     cout << "Please enter an integer value >> ";
-    cin >> i; //cin : standard input (keyboard)
+    cin >> i; //cin : standard input (keyboard stream object)
+    // value is stored when the enter key is pressed
 
      // in the case of the one not surrouded by quotation marks, it means the value of the variable
-    cout << "The value you entered is " << i ;
+    cout << "The value you entered is " << i ; // prints the value of i on screen
     cout << "\nand its double is " << i*2 << ".\n";
     return 0;
 
 }
+
+// Compilation : C++ compiler generates executable file(machine code) from C++ source code
+// > g++ -o TestProg TestProg.cpp
+// > ./TestProg
+
