6380dbc042146fc74415da7b678f373f5f84bd38
Jimin Kim <0305jimin@gmail.com>
2022-10-20 16:46:42 +0900

    Docs: annotating; mode of file opening

--- Introduction to CPP/07.cpp
+++ Introduction to CPP/07.cpp
@@ -6,6 +6,11 @@ int main() {
     ofstream myfile ("example.txt");
     
     if (myfile.is_open()){
+        /* 
+            in fact, since the mode of ofstream is ios::out, 
+            if there's no corresponding file, 
+            a new file is created, no error is being occured
+        */
         myfile << "This is a line.\n";
         myfile << "This is another line.\n";
         myfile.close();
