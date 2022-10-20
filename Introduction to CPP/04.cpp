c06820b22714054b60e11a386641c4cefbe3154c
Jimin Kim <0305jimin@gmail.com>
2022-10-20 14:05:09 +0900

    Docs: annotating; pointer and referencing

--- Introduction to CPP/04.cpp
+++ Introduction to CPP/04.cpp
@@ -7,6 +7,7 @@ class Rectangle {
     public: // member functions
 
         // two class constructors - automatically called when the object is created
+        // constructors have no return type/ value
         Rectangle() {width = 1; height = 1; } 
         Rectangle(int x, int y) : width(x), height(y) {}
         int area() {return width*height;};// can insert the function body(implementation)
@@ -15,18 +16,23 @@ class Rectangle {
 int main(){
     Rectangle obj(3,4);
     Rectangle *foo, *bar, *baz;
+
+    // giving a another name(alias, nickname) to the object
     foo = &obj; // & : 'address of', * : 'value at the address of'
+
+    // naming the object
     bar = new Rectangle(5,6); // dynamic allocation
     baz = new Rectangle[2]; // array of two Rectangle type elements
+    // constructor without parameters is called
 
     // class(type) objects(variables)
     cout << "obj's area: " << obj.area() << "\n";
-    cout << "*foo's area: " << foo->area() << "\n"; // (*foo).area()
+    cout << "*foo's area: " << foo->area() << "\n"; // (*foo).area() // obj.area()
     cout << "*bar's area: " << bar->area() << "\n";
-    cout << "bar[0]'s area: " << bar[0].area() << "\n";
-    cout << "bar[1]'s area: " << bar[1].area() << "\n";
+    cout << "baz[0]'s area: " << baz[0].area() << "\n";
+    cout << "baz[1]'s area: " << baz[1].area() << "\n";
     delete bar;
-    delete[] bar;
+    delete[] baz;
     
     return 0;
 }
