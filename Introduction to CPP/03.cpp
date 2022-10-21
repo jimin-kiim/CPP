7aba4eb3eff7a0093d89c9c020c6b6388e35b5df
Jimin Kim <0305jimin@gmail.com>
2022-10-22 00:39:29 +0900

    Docs: annotating static, dynamic memory allocation

--- Introduction to CPP/03.cpp
+++ Introduction to CPP/03.cpp
@@ -3,27 +3,34 @@ using namespace std;
 
 // OOP : a set of interating objects
 // class : data structure type that can contain member data and member functions
-class Rectangle {
-    int width, height; // member data, variables, attributes, state
-    public: // member functions, procedures, methods, behaviors
+class Rectangle
+{
+    // member data, variables, attributes, state
+    int width, height;
 
-        // class constructor - automatically called when the object is created
-        // constructor doesn't have any return type
-        Rectangle (int,int); // may leave only the prototype of the function
-        int area() {return width*height;};// can insert the function body(implementation) inside the class
+    // member functions, procedures, methods, behaviors
+public:
+    // class constructor - automatically called when the object is created
+    // constructor doesn't have any return type
+    Rectangle(int, int);                   // may leave only the prototype of the function
+    int area() { return width * height; }; // can insert the function body(implementation) inside the class -> inline function
 };
 
 // can insert the function body outside the class
-Rectangle::Rectangle(int x, int y){
-    width = x;// member variable width. 
+Rectangle::Rectangle(int x, int y)
+{
+    width = x; // member variable width.
     height = y;
+    // normally the codes in the constructor initializes the member variables of a class.
 }
 
-int main(){
-
+int main()
+{
     // class(type) objects(variables)
-    Rectangle rect(3,4), rectb(5,6); // different memory space and address are assigned to each objects
+    Rectangle rect(3, 4), rectb(5, 6); // different memory space and address are assigned to each objects
     // when object is created the constructor is automatically called
+    // static memory allocation
+
     cout << "rect area: " << rect.area() << endl; // message reciever.message
     cout << "rectb area: " << rectb.area() << endl;
     return 0;
--- Introduction to CPP/04.cpp
+++ Introduction to CPP/04.cpp
@@ -2,27 +2,32 @@
 using namespace std;
 
 // class : data structure type that can contain member data and member functions
-class Rectangle {
+class Rectangle
+{
     int width, height; // member data(variables)
-    public: // member functions
-
-        // two class constructors - automatically called when the object is created
-        // constructors have no return type/ value
-        Rectangle() {width = 1; height = 1; } 
-        Rectangle(int x, int y) : width(x), height(y) {}
-        int area() {return width*height;};// can insert the function body(implementation)
+public:                // member functions
+    // two class constructors - automatically called when the object is created
+    // constructors have no return type/ value
+    Rectangle()
+    {
+        width = 1;
+        height = 1;
+    }
+    Rectangle(int x, int y) : width(x), height(y) {}
+    int area() { return width * height; }; // can insert the function body(implementation)
 };
 
-int main(){
-    Rectangle obj(3,4);
+int main()
+{
+    Rectangle obj(3, 4);
     Rectangle *foo, *bar, *baz;
 
     // giving a another name(alias, nickname) to the object
     foo = &obj; // & : 'address of', * : 'value at the address of'
 
     // naming the object
-    bar = new Rectangle(5,6); // dynamic allocation
-    baz = new Rectangle[2]; // array of two Rectangle type elements
+    bar = new Rectangle(5, 6); // dynamic allocation
+    baz = new Rectangle[2];    // array of two Rectangle type elements
     // constructor without parameters is called
 
     // class(type) objects(variables)
@@ -31,8 +36,10 @@ int main(){
     cout << "*bar's area: " << bar->area() << "\n";
     cout << "baz[0]'s area: " << baz[0].area() << "\n";
     cout << "baz[1]'s area: " << baz[1].area() << "\n";
+
+    /// delete foo; makes an error (since it's allocated in static memory allocation way, no the dynamically)
     delete bar;
     delete[] baz;
-    
+
     return 0;
 }
