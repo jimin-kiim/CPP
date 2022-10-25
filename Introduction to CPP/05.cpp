eff7551dfc68e8876d2be0965e24a7a7c8397769
Jimin Kim <0305jimin@gmail.com>
2022-10-26 02:47:51 +0900

    Docs: static and dynamic memory allocation

--- Introduction to CPP/05.cpp
+++ Introduction to CPP/05.cpp
@@ -20,32 +20,3 @@ y[0] = 3;
 y[2] = 7;
 
 delete[] y; // deallocating
-
-/* 
-sometimes the static memory allocation is more suitable
-and 
-sometimes the dynamic memmory allocation is more suitable.
-understanding the pros and cons of each of them and using them in pertinent situation is necessary.
-
-# static 
-- advantage : 
-    light-weight.
-    faster to be created and deleted than dynamic allocating method
-    higher speed of the program in the execution time 
-    ( since the memory allocation is already performed at the compile time. )
-- disadvantage : 
-    the exact size of the array must be specified when declaring the array
-
-# dynamic
-- advantage : 
-    no need to specify the exact memory size
-    ( even in the case when I cannot expect the size of array in advance, I can declare the array)
-- disadvantage : 
-    heavy-wight
-    slower speed of the program when executed
-    ( since the prodedure of allocating the memory is performed during the run time)
-
-
-When I have to make a fast program then it's better to use the static memory allocating method
-When I cannot expect the size of the array in advance of executing the program, it's better to use the dynamic memory allocating method
-*/
--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 - [Inline Function](https://github.com/jimin-kiim/CPP/issues/8#issue-1416935867)
 - [Assert Macro Function](https://github.com/jimin-kiim/CPP/issues/4#issue-1416865782)
 - [File Processing](https://github.com/jimin-kiim/CPP/issues/12#issue-1422821074)
+- [Static memory allocation and Dynamic memory allocation](https://github.com/jimin-kiim/CPP/issues/6#issue-1416871517)
