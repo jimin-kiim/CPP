dec8b132a36451d2ea0636099fd4c4ea54ebaab0
Jimin Kim <0305jimin@gmail.com>
2022-12-15 20:17:00 +0900

    Add: dynamic binding example codes

---
+++ example_dynamic_binding/dynamic_binding3.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+
+class Animal { 
+  public: 
+    virtual void eat() 
+    { 
+      std::cout << "I eat like a generic Animal.\n"; 
+    } 
+}; 
+
+class Wolf : public Animal { 
+  public: 
+    void eat() { std::cout << "I eat like a wolf!\n"; } 
+}; 
+
+class Fish : public Animal { 
+  public: 
+    void eat() { std::cout << "I eat like a fish!\n"; } 
+}; 
+
+class OtherAnimal : public Animal { 
+}; 
+
+int main() 
+{ 
+  Animal *anAnimal[4]; 
+  // dynamic binding
+  anAnimal[0] = new Animal(); 
+  anAnimal[1] = new Wolf(); 
+  anAnimal[2] = new Fish(); 
+  anAnimal[3] = new OtherAnimal(); 
+
+  for(int i = 0; i < 4; i++) anAnimal[i]->eat();
+  return 0; 
+} 
